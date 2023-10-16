2fe06bc1ce64fc4bece7f0ab2d9876956b2a78bd
Murata Ugetsu <92856337+UgetsuMurata@users.noreply.github.com>
2023-10-16 16:42:25 +0800

    all I did was love him chariz

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 
 import './index.css';
 import Header from './components/Header';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-
-
-
+      <Header/>
+      <SignIn/>
     </div>
   );
 }
