caa3e4d5f1dbc7b9cf823208effc5dd8c74e13f8
Angelo Echano <grey12angelo@gmail.com>
2023-10-18 21:48:25 +0800

    update homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,18 +1,64 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 function Homepage() {
+  const imageSrc =
+    "https://github.com/aechano/Vicenture-SAD/blob/main/src/res/img/LGU-PERS.jpg?raw=true";
+  const imageAlt = "Mananap Falls";
+  const overlayRef = useRef(null);
+  const [overlayHeight, setOverlayHeight] = useState("auto");
+
+  useEffect(() => {
+    function handleResize() {
+      if (overlayRef.current) {
+        setOverlayHeight(`${overlayRef.current.previousSibling.clientHeight}px`);
+      }
+    }
+
+    handleResize(); // Initial setup
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const containerStyle = {
+    position: "relative",
+  };
+
+  const imageStyle = {
+    width: "100%",
+    objectFit: "cover",
+  };
+
+  const overlayStyle = {
+    position: "absolute",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: overlayHeight,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "white",
+    textTransform: "uppercase",
+  };
+
+  const isSmallScreen = window.innerWidth <= 1180;
+
   return (
-    <div className="relative h-screen w-screen flex items-center justify-center">
+    <div style={containerStyle} className="relative flex items-center justify-center">
       <div className="h-full w-full relative overflow-hidden">
-        <img
-          className="h-auto w-full object-center"
-          src="https://github.com/aechano/Vicenture-SAD/blob/main/src/res/img/mananap.jpg?raw=true"
-          alt="Mananap Falls"
-        />
-        <div className="absolute w-full h-full top-0 left-0 bg-gray-900/50 flex flex-col items-center justify-center text-center text-white tracking-wide">
-            <h1 className="text-6xl font-bold p-4">WELCOME TO SAN VICENTE,</h1>
-            <h1 className="text-6xl font-bold p-4">CAMARINES NORTE</h1>
-            <form className="w-5/12 mt-5">   
+        <img style={imageStyle} src={imageSrc} alt={imageAlt} className="h-screen lg:h-auto" />
+        <div ref={overlayRef} style={overlayStyle}>
+          <h1 className={`lg:text-6xl font-bold p-2 ${isSmallScreen ? "text-2xl" : "text-4xl"}`}>
+            WELCOME TO SAN VICENTE,
+          </h1>
+          <h1 className={`lg:text-6xl font-bold p-2 ${isSmallScreen ? "text-2xl" : "text-4xl"}`}>
+            CAMARINES NORTE
+          </h1>
+            <form className="w-9/12 lg:w-5/12 mt-5">   
                 <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                 <div class="relative">
                     <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -23,7 +69,7 @@ function Homepage() {
                     <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required/>
                     <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
                 </div>
-            </form>
+           </form>
         </div>
       </div>
     </div>
