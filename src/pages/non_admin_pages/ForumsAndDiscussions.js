98b770dd1c05ccf4004c7a729e61d9d60b6b7c87
Murata Ugetsu <92856337+UgetsuMurata@users.noreply.github.com>
2023-10-23 00:31:57 +0800

    update sa fad.js, sorry na ito lang nakayanan muna T-T

--- src/pages/non_admin_pages/ForumsAndDiscussions.js
+++ src/pages/non_admin_pages/ForumsAndDiscussions.js
@@ -7,36 +7,35 @@ import Body from '../../classifiers/Body'
 export default function ForumsAndDiscussions() {
   var contents = [
     {
-      contentID:1042,
-      email:"gheeelo@gmail.com",
-      title:"San Vicente - A Forum Example",
-      body:"San Vicente, a hidden gem nestled in the heart of Camarines Norte, is a place of serene beauty and natural wonder. This tranquil town, often overshadowed by more prominent tourist destinations in the Philippines, holds a secret that is waiting to be discovered - the breathtaking Mananap Falls. San Vicente, with its lush greenery, pristine rivers, and warm, welcoming locals, offers an ideal escape from the hustle and bustle of city life. The town is a haven for nature lovers and adventure seekers, with a wealth of natural attractions to explore. Among these, Mananap Falls stands out as one of the most captivating and enchanting sites in the region. Mananap Falls is a cascading masterpiece of nature, with its crystalline waters tumbling down a series of rocky terraces, creating a mesmerizing visual and auditory spectacle. The falls are surrounded by a lush tropical forest, where exotic flora and fauna thrive, adding to the mystical aura of the place.",
-      images:["image1", "image2"],
-      tags:["tag1", "tag2"]
+      contentID: 1042,
+      email: "gheeelo@gmail.com",
+      title: "San Vicente - A Forum Example",
+      body: "San Vicente, a hidden gem nestled in the heart of Camarines Norte, is a place of serene beauty and natural wonder. This tranquil town, often overshadowed by more prominent tourist destinations in the Philippines, holds a secret that is waiting to be discovered - the breathtaking Mananap Falls. San Vicente, with its lush greenery, pristine rivers, and warm, welcoming locals, offers an ideal escape from the hustle and bustle of city life. The town is a haven for nature lovers and adventure seekers, with a wealth of natural attractions to explore. Among these, Mananap Falls stands out as one of the most captivating and enchanting sites in the region. Mananap Falls is a cascading masterpiece of nature, with its crystalline waters tumbling down a series of rocky terraces, creating a mesmerizing visual and auditory spectacle. The falls are surrounded by a lush tropical forest, where exotic flora and fauna thrive, adding to the mystical aura of the place.",
+      images: [{ src: require("./../../res/img/waterfalls.png"), alt: "waterfalls" }, { src: "", alt: "" }],
+      tags: ["tag1", "tag2"]
     },
     {
-      contentID:1043,
-      email:"gheeelo@gmail.com",
-      title:"San Vicente - A Forum Example 2",
-      body:"A hidden gem nestled in the heart of Camarines Norte, is a place of serene beauty and natural wonder. This tranquil town, often overshadowed by more prominent tourist destinations in the Philippines, holds a secret that is waiting to be discovered - the breathtaking Mananap Falls. San Vicente, with its lush greenery, pristine rivers, and warm, welcoming locals, offers an ideal escape from the hustle and bustle of city life. The town is a haven for nature lovers and adventure seekers, with a wealth of natural attractions to explore. Among these, Mananap Falls stands out as one of the most captivating and enchanting sites in the region. Mananap Falls is a cascading masterpiece of nature, with its crystalline waters tumbling down a series of rocky terraces, creating a mesmerizing visual and auditory spectacle. The falls are surrounded by a lush tropical forest, where exotic flora and fauna thrive, adding to the mystical aura of the place.",
-      images:["", "image2"],
-      tags:["tag1", "tag2"]
+      contentID: 1043,
+      email: "gheeelo@gmail.com",
+      title: "San Vicente - A Forum Example 2",
+      body: "A hidden gem nestled in the heart of Camarines Norte, is a place of serene beauty and natural wonder. This tranquil town, often overshadowed by more prominent tourist destinations in the Philippines, holds a secret that is waiting to be discovered - the breathtaking Mananap Falls. San Vicente, with its lush greenery, pristine rivers, and warm, welcoming locals, offers an ideal escape from the hustle and bustle of city life. The town is a haven for nature lovers and adventure seekers, with a wealth of natural attractions to explore. Among these, Mananap Falls stands out as one of the most captivating and enchanting sites in the region. Mananap Falls is a cascading masterpiece of nature, with its crystalline waters tumbling down a series of rocky terraces, creating a mesmerizing visual and auditory spectacle. The falls are surrounded by a lush tropical forest, where exotic flora and fauna thrive, adding to the mystical aura of the place.",
+      tags: ["tag1", "tag2"]
 
     },
     {
-      contentID:1043,
-      email:"gheeelo@gmail.com",
-      title:"San Vicente - A Forum Example 3",
-      body:"Camarines Norte, is a place of serene beauty and natural wonder. This tranquil town, often overshadowed by more prominent tourist destinations in the Philippines, holds a secret that is waiting to be discovered - the breathtaking Mananap Falls. San Vicente, with its lush greenery, pristine rivers, and warm, welcoming locals, offers an ideal escape from the hustle and bustle of city life. The town is a haven for nature lovers and adventure seekers, with a wealth of natural attractions to explore. Among these, Mananap Falls stands out as one of the most captivating and enchanting sites in the region. Mananap Falls is a cascading masterpiece of nature, with its crystalline waters tumbling down a series of rocky terraces, creating a mesmerizing visual and auditory spectacle. The falls are surrounded by a lush tropical forest, where exotic flora and fauna thrive, adding to the mystical aura of the place.",
-      images:["", ""],
-      tags:["tag1", "tag2"]
+      contentID: 1043,
+      email: "gheeelo@gmail.com",
+      title: "San Vicente - A Forum Example 3",
+      body: "Camarines Norte, is a place of serene beauty and natural wonder. This tranquil town, often overshadowed by more prominent tourist destinations in the Philippines, holds a secret that is waiting to be discovered - the breathtaking Mananap Falls. San Vicente, with its lush greenery, pristine rivers, and warm, welcoming locals, offers an ideal escape from the hustle and bustle of city life. The town is a haven for nature lovers and adventure seekers, with a wealth of natural attractions to explore. Among these, Mananap Falls stands out as one of the most captivating and enchanting sites in the region. Mananap Falls is a cascading masterpiece of nature, with its crystalline waters tumbling down a series of rocky terraces, creating a mesmerizing visual and auditory spectacle. The falls are surrounded by a lush tropical forest, where exotic flora and fauna thrive, adding to the mystical aura of the place.",
+      images: ["", ""],
+      tags: ["tag1", "tag2"]
 
     },
   ]
   return (
     <>
-      <Banner>
-        <p className='text-lgu-lime'>Forums and Discussions</p>
+      <Banner bottomLeft={true} searchBar={true}>
+        <h1>Forums and Discussions</h1>
       </Banner>
       <Body className="mx-5">
         <WebpagePath previous={[{ title: "Home", to: "/" },]} className="pt-5"> Forums and Discussions </WebpagePath>
@@ -47,8 +46,23 @@ export default function ForumsAndDiscussions() {
         </div>
         {
           contents.map((content, index) => (
-            <div key={content.contentID} className='min-h- '>
-
+            <div key={content.contentID} className='flex h-60 bg-gray-100 rounded-3xl my-5 drop-shadow-md hover:brightness-95'>
+              {content.images ?
+                <img
+                  src={content.images[0].src}
+                  alt={content.images[0].alt}
+                  className='h-full w-60 rounded-l-3xl' />
+                :
+                null
+              }
+              <div className='flex-initial w-full ms-5 mt-5'>
+                <h1 className='font-bold'>
+                  {content.title}
+                </h1>
+                <p>
+                  {content.body}
+                </p>
+              </div>
             </div>
           ))
         }
