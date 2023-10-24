1b262d59383357f1ca51ec143a1ff2e58d32b3ce
Murata Ugetsu <92856337+UgetsuMurata@users.noreply.github.com>
2023-10-25 00:15:56 +0800

    handles undefined to in WebpagePath.js

--- src/components/WebpagePath.js
+++ src/components/WebpagePath.js
@@ -3,8 +3,13 @@ import { NavLink } from 'react-router-dom'
 
 export default function WebpagePath(props) {
     return (
-        <div className={'flex '+props.className}>
-            {props.previous.map((page, index) => (<NavLink key={index} to={page.to}>{page.title}/</NavLink>))}
+        <div className={'flex ' + props.className}>
+            {props.previous.map((page, index) => (
+                page.to!==""&&page.to!=undefined?
+                <NavLink key={index} to={page.to}>{page.title}/</NavLink>
+                :
+                <p key={index}>{page.title}/</p>
+            ))}
             <p>{props.children}</p>
         </div >
     )
--- src/pages/non_admin_pages/ForumsAndDiscussions.js
+++ src/pages/non_admin_pages/ForumsAndDiscussions.js
@@ -35,7 +35,7 @@ export default function ForumsAndDiscussions() {
   ]
   return (
     <>
-      <Banner bottomLeft={true} searchBar={true} breadcrumbs={[{ title: "Home", to: PATH_NAME.Home },]}>
+      <Banner bottomLeft={true} searchBar={true} breadcrumbs={[{ title: "Home", to: PATH_NAME.Home },{title:"The Town"}]}>
         <h1>Forums and Discussions</h1>
       </Banner>
       <div className='flex'>
