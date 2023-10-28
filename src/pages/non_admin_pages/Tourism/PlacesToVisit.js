60a5c267163d9fbf8dfe48e2576d34f02fce8657
Murata Ugetsu <92856337+UgetsuMurata@users.noreply.github.com>
2023-10-28 22:46:05 +0800

    stars

--- src/pages/non_admin_pages/Tourism/PlacesToVisit.js
+++ src/pages/non_admin_pages/Tourism/PlacesToVisit.js
@@ -3,7 +3,7 @@ import Banner from '../../../components/Banner'
 import { PATH_NAME } from '../../../Variables/GLOBAL_VARIABLE';
 import { FaFilter } from 'react-icons/fa6'
 import { HiDotsHorizontal } from 'react-icons/hi'
-import {AiOutlineComment} from 'react-icons/ai'
+import { AiOutlineComment } from 'react-icons/ai'
 
 export default function PlacesToVisit() {
     const [search, setSearch] = useState('');
@@ -71,37 +71,52 @@ export default function PlacesToVisit() {
                         </div>
                         <p className="mb-3 font-normal text-stone-500 dark:text-gray-800">Mananap is a 60-feet high waterfalls with a deep swimming pool basin. The place is ideal for swimming, fishing, camping and just getting away from the busy and noisy city life. It is a 2 km hike from the town of Barangay Fabrica in San Vicente, Camarines Norte.</p>
 
-                        <div className="flex items-center space-x-1">
-                            <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        <div className="flex items-center">
+                            <svg className="w-2 h-4 text-yellow-300 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 11 20">
+                                <path d="M12 2 8.91 8.26 2 9.27 7 14.14 5.82 21.02 12 17.77" />
                             </svg>
-                            <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                            <svg className="w-2 h-4 text-yellow-300" aria-hidden="true" fill="currentColor" viewBox="0 0 11 20">
+                                <path d="M0 2 3.09 8.26 10 9.27 5 14.14 6.18 21.02 0 17.77" />
                             </svg>
-                            <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                            <svg className="w-2 h-4 text-yellow-300 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 11 20">
+                                <path d="M12 2 8.91 8.26 2 9.27 7 14.14 5.82 21.02 12 17.77" />
                             </svg>
-                            <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                            <svg className="w-2 h-4 text-yellow-300" aria-hidden="true" fill="currentColor" viewBox="0 0 11 20">
+                                <path d="M0 2 3.09 8.26 10 9.27 5 14.14 6.18 21.02 0 17.77" />
                             </svg>
-                            <svg className="w-4 h-4 text-gray-300 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                            <svg className="w-2 h-4 text-yellow-300 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 11 20">
+                                <path d="M12 2 8.91 8.26 2 9.27 7 14.14 5.82 21.02 12 17.77" />
                             </svg>
-                            
+                            <svg className="w-2 h-4 text-yellow-300" aria-hidden="true" fill="currentColor" viewBox="0 0 11 20">
+                                <path d="M0 2 3.09 8.26 10 9.27 5 14.14 6.18 21.02 0 17.77" />
+                            </svg>
+                            <svg className="w-2 h-4 text-yellow-300 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 11 20">
+                                <path d="M12 2 8.91 8.26 2 9.27 7 14.14 5.82 21.02 12 17.77" />
+                            </svg>
+                            <svg className="w-2 h-4 text-gray-300" aria-hidden="true" fill="currentColor" viewBox="0 0 11 20">
+                                <path d="M0 2 3.09 8.26 10 9.27 5 14.14 6.18 21.02 0 17.77" />
+                            </svg>
+                            <svg className="w-2 h-4 text-gray-300 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 11 20">
+                                <path d="M12 2 8.91 8.26 2 9.27 7 14.14 5.82 21.02 12 17.77" />
+                            </svg>
+                            <svg className="w-2 h-4 text-gray-300" aria-hidden="true" fill="currentColor" viewBox="0 0 11 20">
+                                <path d="M0 2 3.09 8.26 10 9.27 5 14.14 6.18 21.02 0 17.77" />
+                            </svg>
+
                             <div className='flex w-auto h-4 items-center'>
                                 <p className='text-sm font-semibold pl-2'>4.7</p>
                                 <p className='text-sm font-semibold pl-2'>|</p>
                                 <p className='text-sm font-semibold pl-2'>213 votes</p>
 
                                 <div className='flex items-center pl-2'>
-                                    <AiOutlineComment/>
+                                    <AiOutlineComment />
                                     <p className='text-sm font-semibold pl-2'>16 comments</p>
                                 </div>
-                        
+
                             </div>
-                            
+
                         </div>
-                        
+
 
                     </div>
                 </a>
