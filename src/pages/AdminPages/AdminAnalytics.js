1ec96490c3ee55f51eef9857c6daf2618b0a07e2
JosevhelSancho <111216137+JosevhelSancho@users.noreply.github.com>
2023-11-13 22:43:31 +0800

    Updates medyo okay na sa admin, content and preview pa

--- src/pages/AdminPages/AdminAnalytics.js
+++ src/pages/AdminPages/AdminAnalytics.js
@@ -7,7 +7,7 @@ export default function AdminAnalytics({ userType }) {
     {
       id: 'AdminAnalytics',
       label: 'Analytics',
-      content: <div>Web Analytics</div>,
+      content: <div></div>,
     },
   ]);
 
@@ -15,7 +15,7 @@ export default function AdminAnalytics({ userType }) {
     {
       id: 'adminHomepage',
       label: 'Homepage',
-      content: <div>Content for Homepage</div>,
+      content: <h1 className="text-3xl font-bold mb-4 mt-8 ml-4">Homepage edit </h1>,
     },
     {
       id: 'town',
@@ -49,7 +49,7 @@ export default function AdminAnalytics({ userType }) {
     {
       id: 'transparency',
       label: 'Transparency',
-      content: <div>Content for Transparency</div>,
+      content: <h1 className="text-3xl font-bold mb-4 mt-8 ml-4">Transparency Dashboard</h1>,
     },
   ]);
 
@@ -57,7 +57,7 @@ export default function AdminAnalytics({ userType }) {
     {
       id: 'onlineSurvey',
       label: 'Online Survey',
-      content: <div>Content for Online Survey</div>,
+      content: <h1 className="text-3xl font-bold mb-4 mt-8 ml-4">Online Survey</h1>,
     },
   ]);
 
@@ -65,40 +65,50 @@ export default function AdminAnalytics({ userType }) {
     {
       id: 'liveStreaming',
       label: 'Live Streaming',
-      content: <div>Content for Live Streaming</div>,
+      content: <h1 className="text-3xl font-bold mb-4 mt-8 ml-4">Live Streaming</h1>,
     },
     {
       id: 'emergencies',
       label: 'Emergencies',
-      content: <div>Content for Emergencies</div>,
+      content: <h1 className="text-3xl font-bold mb-4 mt-8 ml-4">Emergencies</h1>,
     },
     {
       id: 'reportedContents',
       label: 'Reported Contents',
-      content: <div>Content for Reported Contents</div>,
+      content: <h1 className="text-3xl font-bold mb-4 mt-8 ml-4">Reported Contents</h1>,
     },
   ]);
 
-
-  const [selectedOption, setSelectedOption] = useState(null);
+  const [selectedOption, setSelectedOption] = useState("AdminAnalytics");
   const [selectedTownSubItem, setSelectedTownSubItem] = useState(null);
   const [showTownOptions, setShowTownOptions] = useState(false);
   const [showTourismOptions, setShowTourismOptions] = useState(false);
   const [showInvestOptions, setShowInvestOptions] = useState(false);
+  const [selectedSubItemContent, setSelectedSubItemContent] = useState(null);
+  const [townDropdownOpen, setTownDropdownOpen] = useState(false);
+  const [tourismDropdownOpen, setTourismDropdownOpen] = useState(false);
+  const [investDropdownOpen, setInvestDropdownOpen] = useState(false);
+
 
   const townRef = useRef();
   const tourismRef = useRef();
   const investRef = useRef();
 
+  const dashboardRef = useRef();
+
   useEffect(() => {
     const handleOutsideClick = (event) => {
-      if (
-        !townRef.current?.contains(event.target) &&
-        !tourismRef.current?.contains(event.target) &&
-        !investRef.current?.contains(event.target)
-      ) {
-        setSelectedOption(null);
+      if (!dashboardRef.current.contains(event.target)) {
+        // Set the default selected option based on user type
+        if (userType === USER_TYPES.Admin) {
+          setSelectedOption("AdminAnalytics");
+        } else if (userType === USER_TYPES.LGU) {
+          setSelectedOption("adminHomepage");
+        }
         setSelectedTownSubItem(null);
+        setShowTownOptions(false);
+        setShowTourismOptions(false);
+        setShowInvestOptions(false);
       }
     };
 
@@ -107,47 +117,50 @@ export default function AdminAnalytics({ userType }) {
     return () => {
       document.removeEventListener('click', handleOutsideClick);
     };
-  }, []);
-
+  }, [userType]);
   const handleOptionClick = (option) => {
-    setSelectedOption(option);
-    setSelectedTownSubItem(null);
-
-    if (option.id === 'town') {
-      setShowTownOptions((prev) => !prev);
-      setShowTourismOptions(false);
-      setShowInvestOptions(false);
-    } else if (option.id === 'tourism') {
-      setShowTownOptions(false);
-      setShowTourismOptions((prev) => !prev);
-      setShowInvestOptions(false);
-    } else if (option.id === 'invest') {
-      setShowTownOptions(false);
-      setShowTourismOptions(false);
-      setShowInvestOptions((prev) => !prev);
-    } else {
-      setShowTownOptions(false);
-      setShowTourismOptions(false);
-      setShowInvestOptions(false);
-    }
+    setSelectedOption((prevOption) => {
+      if (prevOption === option.id) {
+        // If the same option is clicked again, reset the state
+        setShowTownOptions(false);
+        setShowTourismOptions(false);
+        setShowInvestOptions(false);
+        setTownDropdownOpen(false);
+        setTourismDropdownOpen(false);
+        setInvestDropdownOpen(false);
+        
+        return '';
+      }
+  
+      setShowTownOptions(option.id === 'town');
+      setShowTourismOptions(option.id === 'tourism');
+      setShowInvestOptions(option.id === 'invest');
+      setTownDropdownOpen(option.id === 'town');
+      setTourismDropdownOpen(option.id === 'tourism');
+      setInvestDropdownOpen(option.id === 'invest');
+  
+      return option.id;
+    });
   };
+  
 
-  const handleTownSubItemClick = (subItem) => {
+  const handleTownSubItemClick = (subItem, event) => {
+    event.stopPropagation();  // Stop event propagation to prevent closing
     setSelectedTownSubItem(subItem);
+    setSelectedSubItemContent(subItem.content);
   };
+  
+  
 
   const renderSubItems = (subItems, ref, option) => {
     const isTownOption = option.id === 'town';
     const isTourismOption = option.id === 'tourism';
     const isInvestOption = option.id === 'invest';
-
+  
     return (
       <div className={`ml-6 ${isTownOption || isTourismOption || isInvestOption ? 'dropdown-submenu' : ''}`} ref={ref}>
-        {(isTownOption && showTownOptions) || (isTourismOption && showTourismOptions) || (isInvestOption && showInvestOptions) ? (
+        {(isTownOption && townDropdownOpen) || (isTourismOption && tourismDropdownOpen) || (isInvestOption && investDropdownOpen) ? (
           <div className="dropdown">
-            <button className="dropbtn" onClick={() => handleOptionClick(option)}>
-              {option && option.label}
-            </button>
             <div className="dropdown-content">
               {/* Filter out title items */}
               {subItems
@@ -156,7 +169,7 @@ export default function AdminAnalytics({ userType }) {
                   <div
                     key={subItem.label}
                     className={`text-base text-lgu-green cursor-pointer font-normal ${selectedTownSubItem === subItem ? 'normal' : 'normal'}`}
-                    onClick={() => handleTownSubItemClick(subItem)}
+                    onClick={(event) => handleTownSubItemClick(subItem, event)}
                   >
                     {subItem.label}
                   </div>
@@ -167,64 +180,33 @@ export default function AdminAnalytics({ userType }) {
       </div>
     );
   };
+
   const renderContent = () => {
-    if (selectedOption && selectedOption.id === 'AdminAnalytics') {
-      return renderAnalyticsContent();
+    const lowerCaseSelectedOption = selectedOption.toLowerCase(); // Convert to lowercase for case-insensitive comparison
+
+    if (lowerCaseSelectedOption === 'adminanalytics') {
+      return <RenderAnalyticsContent />;
     }
 
-    if (selectedOption && !selectedOption.isTitle) {
-      const selectedItem = [...dashboardItems, ...pagesItems, ...formsItems, ...othersItems].find(
-        (item) => item.id === selectedOption.id
-      );
-      return selectedItem ? (
-        <div className="flex">
-          {selectedItem.content}
-        </div>
-      ) : null;
-    }
-
-    return null;
-  };
-
-  const renderAnalyticsContent = () => {
-    return (
-      <div className="flex">
-        {/* First container */}
-        <div className="w-10/11 ml-28">
-          {/* Container for First-Level and Investment-Related Activity */}
-          <div className="border-2 border-lgu-green w-80 h-80 rounded-md ml-36 mt-16 mb-8 font-bold text-center">
-            <p className="mt-4">First-Level User Activity</p>
-          </div>
-
-          <div className="border-2 border-lgu-green w-80 h-80 rounded-md ml-36 mt-8 mb-8 font-bold text-center">
-            <p className="mt-4">New User Accounts</p>
-          </div>
-        </div>
-
-        {/* Second container */}
-        <div className="w-10/11 ml-16">
-          {/* Container for New User Account and User Account */}
-          <div className="border-2 border-lgu-green w-80 h-80 rounded-md ml-2 mt-16 mb-8 font-bold text-center">
-            <p className="mt-4">Investment Related Activity</p>
-          </div>
-
-          <div className="border-2 border-lgu-green w-80 h-80 rounded-md ml-2 mt-8 mb-8 font-bold text-center">
-            <p className="mt-4">User Account</p>
-          </div>
-        </div>
-      </div>
+    const selectedItem = [...dashboardItems, ...pagesItems, ...formsItems, ...othersItems].find(
+      (item) => item.id.toLowerCase() === lowerCaseSelectedOption
     );
+
+    return selectedItem ? (
+      <div className="flex">
+        {selectedItem.content}
+      </div>
+    ) : null;
   };
 
-
-  if ([USER_TYPES.Citizen, USER_TYPES.Guest, USER_TYPES.Tourist, USER_TYPES.Investorm, USER_TYPES.LguSV].includes(userType)) {
-    return <Page403 />
+  if ([USER_TYPES.Citizen, USER_TYPES.Guest, USER_TYPES.Tourist, USER_TYPES.Investor, USER_TYPES.LguSV].includes(userType)) {
+    return <Page403 />;
   }
 
   return (
-    <div className="flex">
-      <div className="bg-lgu-yellow w-1/6 p-4">
-        <div className="text-xl font-bold text-80733D mt-6">Dashboard</div>
+    <div className="flex" ref={dashboardRef}>
+      <div className="bg-lgu-yellow w-1/5 p-4">
+        <div className="text-xl font-bold text-80733D mt-4">Dashboard</div>
         {dashboardItems.map((item) => (
           <div key={item.id} className="cursor-pointer text-base text-lgu-green font-bold ml-4" onClick={() => handleOptionClick(item)}>
             {item.label}
@@ -257,6 +239,37 @@ export default function AdminAnalytics({ userType }) {
       </div>
 
       {renderContent()}
+    </div>
+  );
+}
+
+function RenderAnalyticsContent() {
+  return (
+    <div className="flex">
+      <h1 className="text-3xl font-bold mb-4 mt-8 ml-4">Web Analytics</h1>
+      {/* First container */}
+      <div className="w-10/11 ml-18 mt-4">
+        {/* Container for First-Level and Investment-Related Activity */}
+        <div className="border-2 border-lgu-green w-80 h-80 rounded-md ml-8 mt-16 mb-8 font-bold text-center">
+          <p className="mt-4">First-Level User Activity</p>
+        </div>
+
+        <div className="border-2 border-lgu-green w-80 h-80 rounded-md ml-8 mt-8 mb-8 font-bold text-center">
+          <p className="mt-4">New User Accounts</p>
+        </div>
+      </div>
+
+      {/* Second container */}
+      <div className="w-10/11 ml-16 mt-4">
+        {/* Container for New User Account and User Account */}
+        <div className="border-2 border-lgu-green w-80 h-80 rounded-md ml-2 mt-16 mb-8 font-bold text-center">
+          <p className="mt-4">Investment Related Activity</p>
+        </div>
+
+        <div className="border-2 border-lgu-green w-80 h-80 rounded-md ml-2 mt-8 mb-8 font-bold text-center">
+          <p className="mt-4">User Account</p>
+        </div>
+      </div>
     </div>
   );
 }
