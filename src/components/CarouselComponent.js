051441373a8d1b6d9f1c003559d7693fb787d859
Murata Ugetsu <92856337+UgetsuMurata@users.noreply.github.com>
2023-11-29 14:45:21 +0800

    carousel change

--- src/components/CarouselComponent.js
+++ src/components/CarouselComponent.js
@@ -1,4 +1,3 @@
-// Carousel.js
 import React, { useState, useEffect } from 'react';
 import { Transition } from '@headlessui/react';
 
@@ -38,23 +37,7 @@ const CarouselComponent = ({ images, autoSlideInterval = 3000 }) => {
                 </div>
             </div>
 
-            <button className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" onClick={prevSlide}>
-                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-lgu-green group-hover:bg-green-600 group-focus:ring-4 group-focus:ring-green-500 group-focus:outline-none">
-                    <svg class="w-4 h-4 text-white tl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
-                    </svg>
-                    <span class="sr-only">Previous</span>
-                </span>
-            </button>
-
-            <button className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" onClick={nextSlide}>
-                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-lgu-green group-hover:bg-green-600 group-focus:ring-4 group-focus:ring-green-500 group-focus:outline-none">
-                    <svg class="w-4 h-4 text-white rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
-                    </svg>
-                    <span class="sr-only">Next</span>
-                </span>
-            </button>
+            {/* Arrow buttons removed */}
         </div>
     );
 };
