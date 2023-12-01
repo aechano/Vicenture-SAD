754a5f919dab47e8b4275979375dfcc5a938622f
Murata Ugetsu <92856337+UgetsuMurata@users.noreply.github.com>
2023-12-01 23:52:23 +0800

    new apis

--- src/Variables/GLOBAL_VARIABLE.js
+++ src/Variables/GLOBAL_VARIABLE.js
@@ -8,26 +8,26 @@ const PATH_NAME = {
         },
         list: ["/sign-in", "/sign-up", "/sign-up/lgu", "/sign-up/investor"]
     },
-    AdminPages:{
+    AdminPages: {
         Admin: "/admin",
         Analytics: "analytics",
         TheTown: "the-town",
         MunicipalityProfile: "municipality-profile",
         ElectedOfficials: "elected-officials",
-        Offices:"offices",
-        CitizensCharter:"citizen-charter",
-        Tourism:"tourism",
-        PlacesToVisit:"places-to-visit",
-        Activities:"activities",
-        Invest:"invest",
-        InvestmentOpportunities:"investment-opportunities",
-        ReasonsToInvest:"reasons-to-invest",
-        OnlineSurvey:"online-survey",
-        Emergency:"emergency",
-        Transparency:"transparency",
-        ReportedContents:"reported-contents",
-        GeneralArticles:"general-articles",
-        SportsTourismArticles:"sports-tourism"
+        Offices: "offices",
+        CitizensCharter: "citizen-charter",
+        Tourism: "tourism",
+        PlacesToVisit: "places-to-visit",
+        Activities: "activities",
+        Invest: "invest",
+        InvestmentOpportunities: "investment-opportunities",
+        ReasonsToInvest: "reasons-to-invest",
+        OnlineSurvey: "online-survey",
+        Emergency: "emergency",
+        Transparency: "transparency",
+        ReportedContents: "reported-contents",
+        GeneralArticles: "general-articles",
+        SportsTourismArticles: "sports-tourism"
     },
     Home: "/",
     TheTown: {
@@ -47,9 +47,9 @@ const PATH_NAME = {
         SanVicente: "/tourism/san-vicente-tourism",
         OnlineForm: "/tourism/san-vicente-tourism/online-form",
         PlacesToVisit: "/tourism/places-to-visit",
-        PlacesToVisitPost:"/tourism/place-to-visit",
+        PlacesToVisitPost: "/tourism/place-to-visit",
         Activities: "/tourism/activities",
-        ActivitiesPost:"/tourism/activity",
+        ActivitiesPost: "/tourism/activity",
         Content: "/tourism/places-to-visit/content",
     },
 
@@ -58,7 +58,7 @@ const PATH_NAME = {
         InvestmentOpportunities: "/invest/investment-opportunities",
         InvestmentOpportunitiesContent: "/invest/investment-opportunity",
         ReasonsToInvest: "/invest/reasons-to-invest",
-        ReasonsToInvestContent:"/invest/reason-to-invest",
+        ReasonsToInvestContent: "/invest/reason-to-invest",
         InvestContent: "/invest/reasons-to-invest/content",
     },
 
@@ -71,29 +71,36 @@ const PATH_NAME = {
     Survey: "/surveys",
     SampleSurvey: "/sample-survey",
     Services: "/services",
-    Debug: "/debug", 
+    Debug: "/debug",
     Articles: "/articles",
     ArticleContent: "/article/:articleID",
 
-    AddOffices: "/the-town/offices/add-offices", 
+    AddOffices: "/the-town/offices/add-offices",
 }
 
 const SERVER_ADDRESS = "http://localhost:8080"
+const PUBLIC = "/api/v1/public"
+const LGU = "/api/v1/lgu"
+const AUTH = "/api/v1/auth"
+const ADMIN = "/api/v1/admin"
+const GENERAL = "/api/v1/general"
 const API = {
-    SERVER_ADDRESS:"http://localhost:8080",
-    SignUp: SERVER_ADDRESS+"/api/v1/auth/signup",
-    SignIn: SERVER_ADDRESS+"/api/v1/auth/signin",
-    FeedbackPost: SERVER_ADDRESS+"/api/v1/public/feedback",
-    FeedbackGet: SERVER_ADDRESS+"/api/v1/admin/get-feedback",
-    TourismOnlineFormGet: SERVER_ADDRESS+"/api/v1/lgu/tof",
-    TourismOnlineFormPost: SERVER_ADDRESS+"/api/v1/public/tof",
-    Content: function(method){return SERVER_ADDRESS+"/api/v1/lgu/"+method},
-    ContentDelete: function(id){return SERVER_ADDRESS+"/api/v1/lgu/delete/"+id},
-    GetContentID: function(id){return SERVER_ADDRESS+"/api/v1/public/getContent/"+id},
-    GetContentPages: function(type, id){return SERVER_ADDRESS+"/api/v1/public/getContent/"+type+"/"+id}
+    SignUp: SERVER_ADDRESS + AUTH + "/signup",
+    SignIn: SERVER_ADDRESS + AUTH + "/signin",
+    FeedbackPost: SERVER_ADDRESS + PUBLIC + "/feedback",
+    FeedbackGet: SERVER_ADDRESS + ADMIN + "/get-feedback",
+    TourismOnlineFormGet: SERVER_ADDRESS + LGU + "/tof",
+    TourismOnlineFormPost: SERVER_ADDRESS + PUBLIC + "/tof",
+    Content: function (method) { return SERVER_ADDRESS + LGU + method },
+    ContentDelete: function (id) { return SERVER_ADDRESS + LGU + "/delete/" + id },
+    GetContentID: function (id) { return SERVER_ADDRESS + PUBLIC + "/getContent/" + id },
+    GetContentCount: SERVER_ADDRESS + PUBLIC + "/getContent/count",
+    GetContentPages: function (type, id) { return SERVER_ADDRESS + PUBLIC + "/getContent/" + type + "/" + id },
+    contentRating: function (id) { return SERVER_ADDRESS + PUBLIC + "/contentRating/" + id },
+    setContentRating: SERVER_ADDRESS + GENERAL + "/contentRating"
 }
 const SOCKET = {
-    Messaging: SERVER_ADDRESS+"/ws"
+    Messaging: SERVER_ADDRESS + "/ws"
 }
 
 const USER_TYPES = {
@@ -106,7 +113,7 @@ const USER_TYPES = {
     EndUsers: ["SV_CITIZEN", "TOURIST", "INVESTOR", "GUEST"],
     General: ["SV_CITIZEN", "TOURIST", "INVESTOR", "LGU_SV_MAIN", "ADMIN"]
 }
-const NO_FOOTER = [PATH_NAME.ForumsAndDiscussions, PATH_NAME.Debug, PATH_NAME.Messages + "/*", PATH_NAME.AdminPages.Admin+"/*", ...PATH_NAME.Accounts.list];
+const NO_FOOTER = [PATH_NAME.ForumsAndDiscussions, PATH_NAME.Debug, PATH_NAME.Messages + "/*", PATH_NAME.AdminPages.Admin + "/*", ...PATH_NAME.Accounts.list];
 const NO_HEADER = [PATH_NAME.Debug, ...PATH_NAME.Accounts.list];
 
 export { PATH_NAME, API, NO_FOOTER, NO_HEADER, USER_TYPES, SOCKET };
