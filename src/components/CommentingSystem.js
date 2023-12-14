80ef1f8a85247d82cf1b87a4f9dbfa7a19d82fa2
Murata Ugetsu <92856337+UgetsuMurata@users.noreply.github.com>
2023-12-14 09:59:34 +0800

    comment delete

--- src/components/CommentingSystem.js
+++ src/components/CommentingSystem.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { timeAgo } from '../functionHelpers/Time';
 import { API, PATH_NAME, USER_TYPES } from '../Variables/GLOBAL_VARIABLE';
+import { MdDelete } from "react-icons/md";
 import { CiFlag1 } from "react-icons/ci";
 import { useLocation, useNavigate } from 'react-router';
 import Cookies from 'js-cookie';
@@ -18,6 +19,7 @@ export default function CommentingSystem({ contentID }) {
     const [comment, setComment] = useState(); // comment box contents
     const [revealReplies, setRevealReplies] = useState({}); //used to reveal certain amount of replies to a comment
     const [accountType, setAccountType] = useState(USER_TYPES.Guest);
+    const [email, setEmail] = useState("");
     const [comments, setComments] = useState([]);
     const [filter, setFilter] = useState();
 
@@ -26,6 +28,7 @@ export default function CommentingSystem({ contentID }) {
         if (jwt) {
             var payload = jwtDecode(jwt);
             setAccountType(payload.accountType);
+            setEmail(payload.sub);
         } else {
             setAccountType(USER_TYPES.Guest);
         }
@@ -60,7 +63,7 @@ export default function CommentingSystem({ contentID }) {
 
     const handleCommentPublish = () => {
         setAddComment(false);
-        if (handleCheckForProfanity()){
+        if (handleCheckForProfanity()) {
             alert(`Oops! It looks like there might be some inappropriate language in your comment (word: ${filter.getProfanity(comment)}). If you believe this is a mistake, please let us know.`)
             setComment("");
             return;
@@ -81,6 +84,18 @@ export default function CommentingSystem({ contentID }) {
             })
     }
 
+    const handleDelete = (comment) => {
+        axios.post(API.deleteComment(comment.commentID), {}, {
+            headers: {
+                "Authorization": `Bearer ${Cookies.get("token")}`
+            },
+            withCredentials: true
+        })
+        var new_comments = comments;
+        new_comments.splice(new_comments.indexOf(comment), 1);
+        setComment(new_comments);
+    }
+
     return (
         <>
             {/** add comment button */}
@@ -90,7 +105,7 @@ export default function CommentingSystem({ contentID }) {
                     onClick={() => {
                         setAddComment(!addComment)
                         setComment("");
-                        }}>
+                    }}>
                     + Add a Comment
                 </div>
                 :
@@ -115,8 +130,9 @@ export default function CommentingSystem({ contentID }) {
                         <div className='p-2 outline outline-2 outline-lgu-yellow rounded-full select-none cursor-pointer'
                             onClick={() => {
                                 setAddComment(false);
-                                setComment("");}}
-                            >Cancel</div>
+                                setComment("");
+                            }}
+                        >Cancel</div>
                         <div className='p-2 ml-3 bg-lgu-yellow rounded-full select-none cursor-pointer'
                             onClick={handleCommentPublish}
                         >Comment</div>
@@ -131,7 +147,14 @@ export default function CommentingSystem({ contentID }) {
                 {comments.length > 0 ?
                     comments.map((comment, index) => {
                         return (
-                            <Comment key={index} comment={comment} revealReplies={revealReplies} setRevealReplies={modifyRevealedReplies} isGuest={accountType === USER_TYPES.Guest} />
+                            <Comment
+                                key={index}
+                                comment={comment}
+                                revealReplies={revealReplies}
+                                setRevealReplies={modifyRevealedReplies}
+                                isGuest={accountType === USER_TYPES.Guest}
+                                currentEmail={email}
+                                handleDelete={() => handleDelete(comment)} />
                         );
                     })
                     :
@@ -143,13 +166,14 @@ export default function CommentingSystem({ contentID }) {
         </>
     )
 }
-function Comment({ comment, revealReplies, setRevealReplies, isGuest }) {
+function Comment({ comment, revealReplies, setRevealReplies, isGuest, currentEmail, handleDelete }) {
     const [replyBox, setReplyBox] = useState(false); // used for revealing the reply box
     const [reply, setReply] = useState() // reply box contents
     const [report, setReport] = useState(false) // used for revealing report box
     const [vote, setVote] = useState(0)
     const [initialVote, setInitialVote] = useState(7)
     const location = useLocation();
+    const ownComment = currentEmail === comment.commenter.email;
 
     const handleReport = () => {
         axios.post(API.reportContent, {
@@ -200,9 +224,18 @@ function Comment({ comment, revealReplies, setRevealReplies, isGuest }) {
                             {report ?
                                 <div
                                     className='flex ml-2 select-none cursor-pointer hover:brightness-95 text-sm'
-                                    onClick={handleReport}>
-                                    <CiFlag1 />
-                                    <p>Report</p>
+                                    onClick={ownComment ? handleDelete : handleReport}>
+                                    {ownComment ?
+                                        <>
+                                            <MdDelete />
+                                            <p>Delete</p>
+                                        </>
+                                        :
+                                        <>
+                                            <CiFlag1 />
+                                            <p>Report</p>
+                                        </>
+                                    }
                                 </div>
                                 :
                                 null
@@ -212,7 +245,120 @@ function Comment({ comment, revealReplies, setRevealReplies, isGuest }) {
                     <tr>
                         <td></td> {/** Keep the cells below the image empty */}
                         <td className=''>{comment.comment}</td> {/** Display the first 400 characters of the body, trim it if it ends with a space, and add an ellipsis */}
-                        {/** INSERT CODE FOR COMMENTING SYSTEM REPLY */}
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <div className='flex mt-5'>
+                                {/** Up/Down voting system */}
+                                <div className='flex bg-lgu-yellow w-fit p-2 rounded-full select-none'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                                        className={'w-5 h-5 cursor-pointer bg-lgu-yellow rounded-full ' + (!isGuest ? 'hover:brightness-95 ' : '') + (vote === 1 ? 'brightness-90' : '')}
+                                        fill="none" stroke="#000000" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"
+                                        onClick={() => {
+                                            if (!isGuest) {
+                                                vote === 1 ? setVote(0) : setVote(1)
+                                            }
+                                        }}>
+                                        <circle cx="12" cy="12" r="10" />
+                                        <path d="M16 12l-4-4-4 4M12 16V9" />
+                                    </svg> {/** upvote icon */}
+                                    <p className='text-sm mx-2'>{initialVote + vote}</p> {/** vote count */}
+                                    <svg xmlns="http://www.w3.org/2000/svg" className={'w-5 h-5 cursor-pointer bg-lgu-yellow rounded-full ' + (!isGuest ? 'hover:brightness-95 ' : '') + (vote === -1 ? 'brightness-90' : '')} viewBox="0 0 24 24"
+                                        fill="none" stroke="#000000" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"
+                                        onClick={() => {
+                                            if (!isGuest) {
+                                                vote === -1 ? setVote(0) : setVote(-1)
+                                            }
+                                        }}>
+                                        <circle cx="12" cy="12" r="10" />
+                                        <path d="M16 12l-4 4-4-4M12 8v7" />
+                                    </svg> {/** downvote icon */}
+                                </div>
+                                {/** Comment info display */}
+                                {
+                                    !isGuest ?
+                                        <div
+                                            className='flex bg-lgu-yellow w-fit p-2 ml-10 rounded-full select-none cursor-pointer hover:brightness-95'
+                                            onClick={() => setReplyBox(!replyBox)}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" className='w-5 h-5' viewBox="0 0 24 24"
+                                                fill="none" stroke="#000000" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+                                                <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
+                                            </svg>
+                                            <p className='text-sm mx-2'>Reply</p>
+                                        </div>
+                                        :
+                                        null
+                                }
+                            </div>
+                            {
+                                replyBox ?
+                                    <div className='border-l-2 border-lgu-green mb-3 ps-3'>
+                                        <div className='bg-gray-200 h-20 w-11/12 rounded-3xl mt-10'>
+                                            <textarea className='w-full h-full rounded-3xl p-3 resize-none'
+                                                placeholder='Reply...'
+                                                value={reply}
+                                                onChange={(e) => setReply(e.target.value)} />
+                                        </div>
+                                        <div className='flex mt-3'>
+                                            <div className='p-2 outline outline-2 outline-lgu-yellow rounded-full select-none cursor-pointer' onClick={() => setReplyBox(false)}>Cancel</div>
+                                            <div className='p-2 ml-3 bg-lgu-yellow rounded-full select-none cursor-pointer'
+                                                onClick={() => {
+                                                    setReplyBox(false);
+                                                    /** TODO: Save to database */
+                                                }}
+                                            >Comment</div>
+                                        </div>
+                                    </div>
+                                    :
+                                    null
+                            }
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <div className='border-l-2 border-lgu-green'>
+                                {
+                                    comment?.replyID?.map((reply, index) => {
+                                        if (index < revealReplies[comment.commentID]) {
+                                            return (<Reply reply={reply} key={index} className="mt-5 ml-10" isGuest={isGuest} />);
+                                        } else if (index === revealReplies[comment.commentID]) {
+                                            return (
+                                                <div
+                                                    className='flex ps-10 bg-gray-100 cursor-pointer'
+                                                    onClick={() => setRevealReplies(comment.commentID, revealReplies[comment.commentID] + 5)}
+                                                >
+                                                    <svg xmlns="http://www.w3.org/2000/svg" className='w-6 h-6 stroke-lgu-green' viewBox="0 0 24 24"
+                                                        fill="none" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+                                                        <circle cx="12" cy="12" r="10" />
+                                                        <line x1="12" y1="8" x2="12" y2="16" />
+                                                        <line x1="8" y1="12" x2="16" y2="12" />
+                                                    </svg>
+                                                    <p className='my-auto ml-2'>{comment.replyID.length - revealReplies[comment.commentID]} more...</p>
+                                                </div>
+                                            );
+                                        } else return null;
+                                    })
+                                }
+                                {
+                                    comment?.replyID?.length < revealReplies[comment.commentID] && comment?.replyID?.length > 3 ?
+                                        <div
+                                            className='flex ps-10 bg-gray-100 cursor-pointer'
+                                            onClick={() => setRevealReplies(comment.commentID, 1)}
+                                        >
+                                            <svg xmlns="http://www.w3.org/2000/svg" className='w-6 h-6 stroke-lgu-green' viewBox="0 0 24 24"
+                                                fill="none" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+                                                <circle cx="12" cy="12" r="10" />
+                                                <line x1="8" y1="12" x2="16" y2="12" />
+                                            </svg>
+                                            <p className='my-auto ml-2'>Show less...</p>
+                                        </div>
+                                        :
+                                        null
+                                }
+                            </div>
+                        </td>
                     </tr>
                 </tbody>
             </table>
